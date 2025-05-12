9a6e7be5fcb991e074b9ec0a6062941a77a4351e
nanahoinoue <na.a.ho.10.3.7@gmail.com>
2025-05-12 22:17:07 +0900

    kadai_007

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang='ja'>
+
+<head>
+    <meta charset="UTF-8">
+    <title>課題</title>
+</head>
+
+<body>
+    <p>
+    <?php
+    //連想配列に代入
+    $personal_data = ['name' => 'onion','price' => 200,'weight' => 160];
+
+    //連想配列の値出力
+    print_r($personal_data);
+    ?>
+</p>
+</body>
+</html>
